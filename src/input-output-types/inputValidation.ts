786dd26e312ac32053b153195512ec4ae946ce5d
leha- vitor96 <viiiitorr96@gmail.com>
2025-03-04 16:36:27 +0900

    first commit

--- src/input-output-types/inputValidation.ts
+++ src/input-output-types/inputValidation.ts
@@ -47,6 +47,13 @@ export const putInputValidation = (video: putInputVideoType) => {
             message: 'error!!!', field: 'author'
         })
     }
+
+    if (typeof video.canBeDownloaded !== 'boolean') {
+        errors.errorsMessages.push({
+            message: 'error!!!', field: 'canBeDownloaded'
+        })
+    }
+
     if (video.minAgeRestriction !== null && !Number.isFinite(video.minAgeRestriction) || video.minAgeRestriction! > 18 || video.minAgeRestriction !== null && video.minAgeRestriction < 1) {
         errors.errorsMessages.push({
             message: 'error!!!', field: 'minAgeRestriction'
